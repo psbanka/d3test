f8d2a53c69d0b8877180420c3c9b1161161ed024
Peter Banka <pbanka@fastly.com>
2017-12-07 21:36:22 -0800

    working test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import RD3 from './RD3'
+
+
+class App extends Component {
+  render() {
+    return (
+      <RD3/>
+    )
+  }
+}
+
+export default App;
